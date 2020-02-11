3aa7a0e8ed87d38a26e6a4a1325dcb8827afff09
Eric Washington <mrkimjarrow95@gmail.com>
2020-02-10 19:04:27 -0600

    Added Non ascii type activity

---
+++ Chapter1/Exercise3/ArithmeticOperations.java
@@ -0,0 +1,7 @@
+public class ArithmeticOperations{
+	public static void main(String[] args){
+		System.out.println("Non-ASCII characters: ☺");            
+		System.out.println("∀x ∈ ℝ: ⌈x⌉ = −⌊−x⌋");
+		System.out.println("π ≅ " + 3.1415926535);
+	}
+}
